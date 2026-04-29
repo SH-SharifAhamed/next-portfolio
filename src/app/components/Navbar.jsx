d6a340e456db41a73d47ecdc639f96eb59b23ca1
SH-SharifAhamed <sharif845g7@gmail.com>
2026-04-29 18:19:42 +0600

    Navbar clear done

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,124 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { FaBars, FaGithub, FaTimes } from "react-icons/fa";
+import { useState } from "react";
+
+const Navbar = () => {
+
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="border-b bg-[#191924]">
+      <nav className=" flex justify-between items-center px-4 py-3 max-w-6xl mx-auto">
+        <div className="flex gap-2 items-center">
+          <Image
+            src={"/logo.png"}
+            alt="logo"
+            loading="eager"
+            width={40}
+            height={40}
+            className="object-cover h-auto w-auto"
+          />
+        </div>
+
+        <ul className="hidden md:flex items-center gap-6 text-sm text-white">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/about"}>About</Link>
+          </li>
+          <li>
+            <Link href={"/skills"}>Skills</Link>
+          </li>
+          <li>
+            <Link href={"/experience"}>Experience</Link>
+          </li>
+          <li>
+            <Link href={"/projects"}>Projects</Link>
+          </li>
+          <li>
+            <Link href={"/education"}>Education</Link>
+          </li>
+        </ul>
+
+        <div className="hidden md:flex items-center text-blue-600 hover:bg-blue-500 
+        hover:text-white transition-colors duration-300 rounded-full">
+          <Link
+            href={"https://github.com/SH-SharifAhamed"}
+            target="_blank"
+            className="flex items-center gap-2 border-2 border-blue-600 px-4 py-2 rounded-full "
+          >
+            <FaGithub className="text-xl" />
+            GitHub Profile
+          </Link>
+        </div>
+
+        <div className="md:hidden cursor-pointer text-white">
+          {open ? (
+            <FaTimes
+              onClick={() => setOpen(false)}
+              className="text-xl text-red-500"
+            />
+          ) : (
+            <FaBars
+              onClick={() => setOpen(true)}
+              className="text-xl text-white"
+            />
+          )}
+        </div>
+      </nav>
+
+      {open && (
+        <div className="md:hidden bg-[#191924] px-6 pb-4 text-white">
+          <ul className="flex flex-col gap-4 text-sm">
+            <li>
+              <Link href="/" onClick={() => setOpen(false)}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link href="/about" onClick={() => setOpen(false)}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/skills" onClick={() => setOpen(false)}>
+                Skills
+              </Link>
+            </li>
+            <li>
+              <Link href="/experience" onClick={() => setOpen(false)}>
+                Experience
+              </Link>
+            </li>
+            <li>
+              <Link href="/projects" onClick={() => setOpen(false)}>
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link href="/education" onClick={() => setOpen(false)}>
+                Education
+              </Link>
+            </li>
+          </ul>
+
+          <Link
+            href="https://github.com/SH-SharifAhamed"
+            target="_blank"
+            className="mt-4 inline-flex items-center gap-2 px-4 py-2 rounded-full 
+            backdrop-blur-md border-2 border-blue-600 text-blue-600
+            hover:bg-blue-500 hover:text-white transition-all duration-300"
+          >
+            <FaGithub />
+            GitHub Profile
+          </Link>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
